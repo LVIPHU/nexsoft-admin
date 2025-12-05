c4b780386a36612f1aaff6f787736a80834d8963
FELIX-LUONG <luongviphu0403@gmail.com>
2025-12-05 17:36:19 +0700

    [update] multiple entry point

--- apps/auth/src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from 'next/navigation'
-import { defaultLanguage } from '@nexsoft-admin/utils'
-
-// Redirect root to default language
-export default function RootPage() {
-  redirect(`/${defaultLanguage}`)
-}
