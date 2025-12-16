d082b43cc68fa561b6ca2521b1b23e8283b0b10f
FELIX-LUONG <luongviphu0403@gmail.com>
2025-12-16 17:35:41 +0700

    [feat] login

--- apps/social/src/main.tsx
+++ apps/social/src/main.tsx
@@ -1,14 +1,20 @@
 import { StrictMode } from 'react';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router';
 import * as ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router';
-import { router } from '@/router';
+import { Test1 } from '@/pages/test1';
 
 async function initApp() {
   const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/callback' element={<Test1 />} />
+          <Route path='/dashboard' element={<Test1 />} />
+          <Route index element={<Navigate replace to='/dashboard' />} />
+        </Routes>
+      </BrowserRouter>
     </StrictMode>,
   );
 }
