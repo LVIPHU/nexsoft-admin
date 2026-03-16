d5ac93e1c520ea57637f236efc70edd5f0ad5d29
FELIX-LUONG <luongviphu0403@gmail.com>
2026-03-16 14:04:55 +0700

    [fix] conditions

--- libs/models/src/violation/violation.ts
+++ libs/models/src/violation/violation.ts
@@ -22,7 +22,7 @@ export const violationSummarySchema = z.object({
 export const violationContentSchema = z.object({
   id: z.number(),
   type: z.string(),
-  content: z.string(),
+  content: z.string().nullish().transform((v) => v ?? ''),
   violation_status: z.string(),
   number_of_violations: z.number(),
   priority: z.string(),
